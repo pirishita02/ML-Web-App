22bb46a269a325c5cc44aab70b21bec45908e88c
Pirishita <pirishita2005@gmail.com>
2025-07-20 13:08:07 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,5 +5,5 @@ st.title('🤖 Machine Learning App')
 
 st.info('This app builds a machine learning model!')
 
-df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor.com/data/master/penguins_cleaned.csv')
+df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
 df
